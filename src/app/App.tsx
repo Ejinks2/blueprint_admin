bf353ff9072b5e982e7be26ac0bd84ccb5eacc3d
miguel-merlin <mmerlin@stevens.edu>
2024-02-22 15:00:17 -0500

    feat: Added new pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
+import DashboardPage from '../pages/DashboardPage'
+import BlogPage from '../pages/BlogPage'
 
 function App (): JSX.Element {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/blog" element={<BlogPage />} />
       </Routes>
     </div>
   )
