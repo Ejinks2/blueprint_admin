c95ad3cb3c1a6538d162284e92c1da4eac6735a5
Terrence Zhang <96602253+ZhangTerrence@users.noreply.github.com>
2024-02-22 19:08:25 -0500

    fix: updated App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,18 @@
-import { Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import DashboardPage from "../pages/DashboardPage";
-import BlogPage from "../pages/BlogPage";
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+import DashboardPage from '../pages/DashboardPage'
+import BlogPage from '../pages/BlogPage'
 
-function App(): JSX.Element {
+function App (): JSX.Element {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/blog" element={<BlogPage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/blog' element={<BlogPage />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
