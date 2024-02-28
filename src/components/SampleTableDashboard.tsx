eb70b3d6d0975bba36a45c9845d1ffe991a8237f
Christopher Engelbart <cengelbart39@protonmail.com>
2024-02-28 18:02:01 -0500

    Add SampleTableDashboard with data loaded from sample_data.json - Move sample_data.json into src/ so it can be used in SampleTableDashboard

---
+++ src/components/SampleTableDashboard.tsx
@@ -0,0 +1,38 @@
+import { Table, Thead, Tbody, Tr, Th, TableContainer } from '@chakra-ui/react'
+import React from 'react'
+import sampleUserData from '../sample_data.json'
+
+function SampleTableDashboard (): JSX.Element {
+  const [sampleData] = React.useState(sampleUserData)
+
+  return (
+    <TableContainer>
+      <Table variant="simple" size='md'>
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Email</Th>
+            <Th>Title</Th>
+            <Th>Status</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {sampleData && Object.values(sampleData.Users)
+            .sort((a, b) => {
+              return a.Displayname.localeCompare(b.Displayname)
+            }).filter((user) => !user.Disabled)
+            .map(({ Displayname, Email, Disabled }) =>
+              <Tr key={Email}>
+                <Th>{Displayname}</Th>
+                <Th>{Email}</Th>
+                <Th></Th>
+                <Th>{Disabled ? 'Inactive' : 'Active'}</Th>
+              </Tr>
+            )}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default SampleTableDashboard
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import { Container, HStack } from '@chakra-ui/react'
 import TableDashboard from '../components/TableDashboard'
 import Sidebar from '../components/Sidebar'
+import SampleTableDashboard from '../components/SampleTableDashboard'
 
 function HomePage (): JSX.Element {
   return (
     <HStack height="100vh" spacing="0">
       <Sidebar />
       <Container>
+        <SampleTableDashboard />
         <TableDashboard />
       </Container>
     </HStack>
