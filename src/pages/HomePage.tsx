3ba975e2fe6fe4d902f93724146bd823d552abc3
miguel-merlin <mmerlin@stevens.edu>
2024-03-28 15:49:03 -0400

    feat: Added alert message on failed API call

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,20 @@
-import { Container, HStack } from '@chakra-ui/react'
+import { Container, HStack, Text } from '@chakra-ui/react'
 import TableDashboard from '../components/TableDashboard'
 import Sidebar from '../components/Sidebar'
 import { getAllUsers } from '../api/lib/users'
 import { type User } from '../types/index'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function HomePage (): JSX.Element {
   const [members, setMembers] = React.useState<User[]>([])
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     void getAllUsers().then((response) => {
       setMembers(response.data as User[])
+    }).catch((error) => {
+      console.error(error)
+      setError('Failed to load Blueprint members. Please try again later.')
     })
   }, [])
 
@@ -18,7 +22,7 @@ function HomePage (): JSX.Element {
     <HStack height="100vh" spacing="0">
       <Sidebar />
       <Container>
-        <TableDashboard members={members} />
+        {error ? (<Text color="red.500">{error}</Text>) : (<TableDashboard members={members} />)}
       </Container>
     </HStack>
   )
