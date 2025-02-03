b16c494e12d674c4b28f220080de0965ebb1d18c
miguel-merlin <mmerlin@stevens.edu>
2025-02-03 11:45:55 -0500

    fix: removed test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
-import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
@@ -16,13 +15,8 @@ if (rootElement !== null) {
           <App />
         </ChakraProvider>
       </BrowserRouter>
-    </React.StrictMode>,
+    </React.StrictMode>
   );
 } else {
   console.error("Failed to find the root element");
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
